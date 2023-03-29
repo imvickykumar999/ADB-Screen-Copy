d45f6cd51392444ef05f1a5b31c7dae2da2fb6ed
imvickykumar999 <imvickykumar999@gmail.com>
2023-03-29 12:04:48 +0530

    Update keyevents.py
    https://youtu.be/uHvtl3Ky0aw

--- keyevents.py
+++ keyevents.py
@@ -1,30 +1,47 @@
 
-# OUTPUT : https://youtube.com/shorts/Tri_bp0lRlQ?feature=share
+# OUTPUT : 
+# https://youtube.com/shorts/Tri_bp0lRlQ?feature=share
+# https://youtu.be/uHvtl3Ky0aw
 
 import os, json
 
-def power_off():
+def keyevent():
     with open("keyevents.json", "r") as json_file:
         my_dict = json.load(json_file)
 
-    # inp = 'key_power' # to power off mobile
-    inp = input('Enter key name : ')
+    inp=1
+    while inp:
+        inp = input('Enter key_ or event (tab=61, enter=66) : ')
+        if inp == '*':
+            break
+        try:
+            # inp = 'key_power' # to power off mobile
+            cmd = my_dict['key_events'][inp]
+            os.system(cmd)
+        except:
+            os.system(f'adb shell input keyevent {inp}')
 
-    cmd = my_dict['key_events'][inp]
-    os.system(cmd)
+def typing():
+    inp = 26 # to power off mobile
+    while inp:
+        inp = input('Enter a Sentence : ')
+        if inp == '*':
+            break
+        for i in inp:
+            try:
+                res = int(i) + 7
+            except:
+                if i == ' ':
+                    res = 62
+                else:
+                    res = ord(i) - 68
+            finally:
+                os.system(f"adb shell input keyevent {res}")
+        os.system(f"adb shell input keyevent 66") # enter key
 
-
-inp = 26 # to power off mobile
-while inp:
-    inp = input('Enter a Sentence : ')
-
-    for i in inp:
-        if i == ' ':
-            res = 62
-        else:
-            res = ord(i) - 68
-
-        os.system(f"adb shell input keyevent {res}")
-    os.system(f"adb shell input keyevent 66") # enter key
-
-power_off()
+print('\nEnter * to break anytime.')
+while 1:
+    print('\nWelcome to Typing ...')
+    typing()
+    print('\nWelcome to key_events.')
+    keyevent()
