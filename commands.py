909c062802d97b1925f5c0367883a8b5ee80ea18
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-21 15:56:10 +0530

    easy typing

---
+++ commands.py
@@ -0,0 +1,27 @@
+
+'''
+USB DEBUG:
+---------
+https://stackoverflow.com/a/35642973/11493297
+
+ADB Shell commands:
+------------------
+adb shell input text '\"'
+adb shell input text "\'"
+
+OUTPUT:
+------ 
+https://youtube.com/shorts/Tri_bp0lRlQ
+https://youtube.com/shorts/uHvtl3Ky0aw
+'''
+
+import os
+
+while True:
+    text = input('>>> ').split()
+
+    for i in text:
+        os.system(f'''adb shell input text "{i}"''')
+        os.system(f"adb shell input keyevent 62")
+
+    os.system(f"adb shell input keyevent 66")
