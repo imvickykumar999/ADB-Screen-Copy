d48aca6c0c1c8bd002e992f102ef7cfc420879d5
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-24 15:06:06 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -2,12 +2,21 @@
 '''
 USB DEBUG:
 ---------
-https://stackoverflow.com/a/35642973/11493297
 
-ADB Shell commands:
-------------------
+USB Troubleshoot: https://stackoverflow.com/a/35642973/11493297
+Typing over Wifi: https://stackoverflow.com/a/42364486/11493297
+
+Exception : ADB Shell commands:
+------------------------------
+
+adb shell input text '\'
+\
+
 adb shell input text '\"'
+"
+
 adb shell input text "\'"
+'
 
 OUTPUT:
 ------ 
@@ -16,15 +25,17 @@ https://youtube.com/shorts/uHvtl3Ky0aw
 '''
 
 import os
-
-while True:
-    text = input('>>> ').split()
+while True: # Ctrl+C to exit.
+    text = input('>>> ')
 
     for i in text:
-        if i == '*':
-            os.system("adb shell input keyevent 17")
-        else:
-            os.system(f'''adb shell input text "{i}"''')
-            
-        os.system("adb shell input keyevent 62")
+        if   i == ' ': os.system("adb shell input keyevent 62")
+        elif i == '*': os.system("adb shell input keyevent 17")
+        elif i == ';': os.system("adb shell input keyevent 74")
+        elif i == '(': os.system("adb shell input keyevent 71")
+        elif i == ')': os.system("adb shell input keyevent 72")
+
+        elif i in ('"', "'"): os.system("adb shell input keyevent 75")
+        else: os.system(f'''adb shell input text {i}''')
+    input('... ')
     os.system("adb shell input keyevent 66")
