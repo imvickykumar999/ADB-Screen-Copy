904cab6bccc956601b039e8de48312d967b818c0
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-05 11:35:51 +0530

    Update xyClick.py

--- ScreenShare/xyClick.py
+++ ScreenShare/xyClick.py
@@ -3,43 +3,46 @@ import os, time
 
 def unlock(code = 2589):
     time.sleep(2)
-    os.system('adb shell input keyevent 26') # Turn ON screen
-    os.system(f'adb shell input text {code}') # Input Passcode
-    os.system('adb shell input keyevent 66') # Press OK
+    os.system('adb shell input keyevent 26')
+    os.system(f'adb shell input text {code}')
+    os.system('adb shell input keyevent 66')
     time.sleep(2)
 
-def click(x, y):
-    print(f'x, y : {x}, {y}')
+def click(x, y, mess = 'click'):
+    print(f'({mess}) : x, y : {x}, {y}')
     os.system(f'adb shell input tap {x} {y}')
 
-# unlock()
 
-click(x = 250, y = 150) # undo
-# click(x = 360, y = 150) # redo
+n = 16
+# unlock() # passed default passcode
+# click(x = 40, y = 1270, mess = 'toggle')
 
-click(x = 80, y = 1210) # toggle
-# for i in range(4): undo()
+# for i in range(n): 
+#     click(x = 250, y = 140, mess = 'undo')
 
-# # row 1st ...
-# click(108, 350) # 1st icon
-# click(275, 350) # 2nd icon
-# click(444, 350) # 3rd icon
-# click(610, 350) # 4th icon
+# for i in range(n): 
+#     click(x = 360, y = 140, mess = 'redo')
 
-# # row 2nd ...
-# click(108, 600) # 5th icon
-# click(275, 600) # 2nd icon
-# click(444, 600) # 3rd icon
-# click(610, 600) # 1st icon
+# row 1st ...
+click(108, 350) # 1st icon
+click(275, 350) # 2nd icon
+click(444, 350) # 3rd icon
+click(610, 350) # 4th icon
 
-# # row 3rd ...
-# click(108, 851) # 5th icon
-# click(275, 851) # 2nd icon
-# click(444, 851) # 3rd icon
-# click(610, 851) # 1st icon
+# row 2nd ...
+click(108, 600) # 5th icon
+click(275, 600) # 2nd icon
+click(444, 600) # 3rd icon
+click(610, 600) # 1st icon
 
-# # row 4th ...
-# click(108, 1100) # 5th icon
-# click(275, 1100) # 2nd icon
-# click(444, 1100) # 3rd icon
-# click(610, 1100) # 1st icon
+# row 3rd ...
+click(108, 851) # 5th icon
+click(275, 851) # 2nd icon
+click(444, 851) # 3rd icon
+click(610, 851) # 1st icon
+
+# row 4th ...
+click(108, 1100) # 5th icon
+click(275, 1100) # 2nd icon
+click(444, 1100) # 3rd icon
+click(610, 1100) # 1st icon
