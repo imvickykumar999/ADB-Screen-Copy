c061308547334d19ac9c96045faa50bec5ec9980
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-07 11:48:07 +0530

    listener

--- Screen/Swipe.py
+++ Screen/Swipe.py
@@ -1,14 +1,28 @@
 
 import os
+from pynput import keyboard
+from pynput.keyboard import Key
 
-x1 = x2 = 350
-y1, y2 = 1100, 200
+out = os.popen('adb shell wm size').read()
+x, y = out.split()[-1].split('x')
 
-# y1, y2 = y2, y1 # swipe down
-os.system(f'adb shell input swipe {x1} {y1} {x2} {y2}') # x1,y1, x2,y2
+x1 = x2 = int(x)/2
+y1, y2 = int(y)*0.65, int(y)*0.15
 
+def on_key_release(key):
+    global x1, y1, x2, y2, cmd
 
-'''
-adb shell wm size
-Physical size: 720x1600
-'''
+    if key == Key.up:
+        print(f'({x1}, {y1}) -> ({x2}, {y2})')
+        cmd = f'adb shell input swipe {x1} {y1} {x2} {y2}'
+
+    elif key == Key.down:
+        print(f'({x1}, {y1}) <- ({x2}, {y2})')
+        cmd = f'adb shell input swipe {x2} {y2} {x1} {y1}'
+
+    elif key == Key.esc:
+        exit()
+    os.system(cmd)
+
+with keyboard.Listener(on_release=on_key_release) as listener:
+    listener.join()
