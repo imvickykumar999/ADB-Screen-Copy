4d3ab3433266072bde6b9438eccfd1699bfde51a
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-14 12:04:38 +0530

    .py 2 .apk

---
+++ Termux/Kivi APK/keyevent.py
@@ -0,0 +1,42 @@
+
+import os, threading
+
+print('\nPress CTRL+PAUSE/BREAK to exit.\n')
+
+def task1():
+    print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
+    print("ID of process running task 1: {}".format(os.getpid()))
+
+    print()
+    while True:
+        x = input('>>> ')
+        os.system(f'adb shell input keyevent {x}')
+
+def task2():
+    print("Task 2 assigned to thread: {}".format(threading.current_thread().name))
+    print("ID of process running task 2: {}".format(os.getpid()))
+
+    print('\n>>>')
+    while True:
+        os.system('scrcpy --tcpip=192.168.0.103')
+  
+if __name__ == "__main__":
+  
+    # print ID of current process
+    print("ID of process running main program: {}".format(os.getpid()))
+  
+    # print name of main thread
+    print("Main thread name: {}".format(threading.current_thread().name))
+  
+    # creating threads
+    t1 = threading.Thread(target=task1, name='t1')
+    t2 = threading.Thread(target=task2, name='t2')  
+  
+    # starting threads
+    t1.start()
+    t2.start()
+  
+    # wait until all threads finish
+    t1.join()
+    t2.join()
+
---
+++ Termux/Kivi APK/main.py
@@ -0,0 +1,9 @@
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='My Name is Vicks.')
+
+TestApp().run()
