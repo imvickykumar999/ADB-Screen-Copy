4153ecf6dc55ee280141342603350fdfd47cadc8
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-16 17:40:02 +0530

    apk installation

--- SDCARD/Pure-Python-ADB/ADB_Class.py
+++ SDCARD/Pure-Python-ADB/ADB_Class.py
@@ -9,35 +9,49 @@ from ppadb.client import Client as AdbClient
 
 class Manuipulate_sdcard():
     def __init__(self, host, port) -> None:
+
+        print(f'\nConnecting ...')
         os.system(f'adb connect {host}')
 
-        self.client = AdbClient(host="127.0.0.1", port=5037)
+        self.client = AdbClient(
+            host="127.0.0.1", 
+            port=5037
+        )
+
         self.serial = f'{host}:{port}'
         self.device = self.client.device(self.serial)
 
 
     def all_devices(self):
-        print([device.serial for device in self.client.devices()])
+        print('\nList of Connected Devices.')
+
+        for device in self.client.devices():
+            print('\t', device.serial)
 
 
     def get_version(self):
-        print(self.client.version())
+        print('\nVersion:', self.client.version())
 
 
     def disconnect(self):
         self.client.remote_disconnect(self.serial)
+        print('\nADB Disconnected.')
 
 
     def screenshot(self):
         result = self.device.screencap()
 
-        with open("static/ppadb.png", "wb") as fp:
+        with open("static/screenshot.png", "wb") as fp:
             fp.write(result)
+            print('\nScreenshot saved.')
 
 
-    # def install_apk(self):
-    #     apk_path = "static/example.apk"
-    #     self.device.install(apk_path)
+    def install_apk(self, apk):
+        apk_path = f"static/{apk}"
+
+        print('\nInstalling APK ...')
+        self.device.install(apk_path)
+        print('APK Installed.')
 
 
     # def shell_echo(self):
@@ -58,8 +72,11 @@ if __name__ == '__main__':
     host, port = "192.168.0.103", 5555
     sdcard = Manuipulate_sdcard(host, port)
 
-    # sdcard.all_devices()
     # sdcard.get_version()
+    sdcard.all_devices()
+
+    apk = 'example.apk'
+    # sdcard.install_apk(apk)
 
     sdcard.screenshot()
     # sdcard.disconnect()
