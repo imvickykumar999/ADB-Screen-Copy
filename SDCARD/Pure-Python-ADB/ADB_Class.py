9ee365784ecd88374b01ca3fb590840665974070
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-16 19:26:14 +0530

    class object called

--- SDCARD/Pure-Python-ADB/ADB_Class.py
+++ SDCARD/Pure-Python-ADB/ADB_Class.py
@@ -1,10 +1,8 @@
 
 # https://github.com/Swind/pure-python-adb#examples
-# Default serial is "127.0.0.1" and 5037
 
-
-import os
 from ppadb.client import Client as AdbClient
+import os
 
 
 class Manuipulate_sdcard():
@@ -33,6 +31,10 @@ class Manuipulate_sdcard():
         print('\nVersion:', self.client.version())
 
 
+    def shell_echo(self):
+        self.device.shell("echo Hello World !")
+
+
     def disconnect(self):
         self.client.remote_disconnect(self.serial)
         print('\nADB Disconnected.')
@@ -54,29 +56,39 @@ class Manuipulate_sdcard():
         print('APK Installed.')
 
 
-    # def shell_echo(self):
-    #     self.device.shell("echo hello world !")
+    def push_folder(self, path='Download/'):
+        self.device.push("static/", f"/sdcard/{path}/static/")
+        print(f'\nFolder {path}/static Pushed.')
 
 
-    # def push_file(self):
-    #     # self.device.push("static/", "/sdcard/Download/Telegram/static/") # push folder
-    #     self.device.push("static/example.apk", "/sdcard/Download/Telegram/example.apk")
+    def push_file(self, file='screenshot.png', path='Download/'):
+        self.device.push(f"static/{file}", f"/sdcard/{path}/{file}")
+        print(f'\nFile {path}/{file} Pushed.')
 
 
-    # def pull_file(self):
-    #     self.device.shell("screencap -p /sdcard/Download/Telegram/DARE2COMPETE HACKATHON.pdf")
-    #     self.device.pull("/sdcard/Download/Telegram/DARE2COMPETE HACKATHON.pdf", "static/DARE2COMPETE HACKATHON.pdf")
+    def pull_file(self, file='screenshot.png', path='Download/'):
+        self.device.shell(f"screencap -p /sdcard/{path}/{file}")
+        
+        self.device.pull(f"/sdcard/{path}/{file}", f"static/{file}")
+        print(f'\nFile {path}/{file} Pulled.')
 
 
 if __name__ == '__main__':
     host, port = "192.168.0.103", 5555
     sdcard = Manuipulate_sdcard(host, port)
 
-    # sdcard.get_version()
+    sdcard.get_version()
     sdcard.all_devices()
 
     apk = 'example.apk'
-    # sdcard.install_apk(apk)
+    sdcard.install_apk(apk)
+
+    path = 'Download/Telegram'
+    file = 'screenshot.png'
+
+    sdcard.push_folder(path)
+    sdcard.pull_file(file, path)
+    sdcard.push_file(file, path)
 
     sdcard.screenshot()
-    # sdcard.disconnect()
+    sdcard.disconnect()
